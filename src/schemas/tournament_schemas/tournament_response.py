a9577f647f48c67f8720bbadc4cca464e7dd8e3b
DanielChachagua <130020250+DanielChachagua@users.noreply.github.com>
2025-01-20 02:04:42 -0300

    Update tournament_response.py

--- src/schemas/tournament_schemas/tournament_response.py
+++ src/schemas/tournament_schemas/tournament_response.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 from pydantic import BaseModel
 from src.models.tournaments import FormatTournament, TypeTournament, StatusTournament
 from src.schemas.organizer_schemas.organizer_dto import OrganizerDTO
